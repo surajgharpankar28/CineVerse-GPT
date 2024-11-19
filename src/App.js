c9c22e56faef5384c585ccd7cbafc5612c6580d5
Suraj Gharpankar <surajgharpankar28@gmail.com>
2024-11-19 16:36:40 +0530

    configured Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl text-orange-500 font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
