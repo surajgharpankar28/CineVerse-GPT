d0574df9f2b0e7f6d4e24ee594b61830703ad3da
Suraj Gharpankar <surajgharpankar28@gmail.com>
2024-11-29 20:47:32 +0530

    -

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -8,17 +8,19 @@ const SecondaryContainer = () => {
 
   return (
     movies.nowPlayingMovies && (
-      <div className="px-2 md:pl-12 mt-0 md:-mt-48 relative z-20 scrollbar-none ">
-        <MovieList
-          title={"Now Playing"}
-          movies={movies.nowPlayingMovies || []}
-        />
-        <MovieList title={"Top Rated"} movies={movies.topRatedMovies || []} />
-        <MovieList title={"Popular"} movies={movies.popularMovies || []} />
-        <MovieList
-          title={"Upcoming Movies"}
-          movies={movies.upcomingMovies || []}
-        />
+      <div className="overflow-clip relative z-20">
+        <div className="overflow-x-hidden px-2 md:pl-12 mt-0 md:-mt-48  scrollbar-none w-full">
+          <MovieList
+            title="Now Playing"
+            movies={movies.nowPlayingMovies || []}
+          />
+          <MovieList title="Top Rated" movies={movies.topRatedMovies || []} />
+          <MovieList title="Popular" movies={movies.popularMovies || []} />
+          <MovieList
+            title="Upcoming Movies"
+            movies={movies.upcomingMovies || []}
+          />
+        </div>
       </div>
     )
   );
